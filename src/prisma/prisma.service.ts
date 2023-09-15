d27927e819dbbee710a843d32a7504a628314408
IgorVeludo2 <velvet.guimaraes@gmail.com>
2023-09-15 16:51:05 -0300

    feat: implement onModuleInit and OnModuleDestroy

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     super({
       log: ['warn', 'error'],
     })
+  }
+
+  onModuleInit() {
+    return this.$connect()
+  }
+
+  /**
+   * If the application crash,
+   * the connection with de database is automaticly closed
+   */
+  onModuleDestroy() {
+    return this.$disconnect()
   }
 }
