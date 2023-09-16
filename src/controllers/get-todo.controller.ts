416db9535e6fe536513306c71386297c034c865c
IgorVeludo2 <velvet.guimaraes@gmail.com>
2023-09-15 21:22:21 -0300

    feat: getAll and getById

--- src/controllers/get-todo.controller.ts
+++ src/controllers/get-todo.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Param,
+} from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { string } from 'zod'
+
+interface RequestById {
+  id: string
+}
 
 @Controller('/todos')
 export class GetTodoContoller {
   constructor(private prisma: PrismaService) {}
 
   @Get()
-  async handle() {
+  async getAll() {
     return await this.prisma.todoTask.findMany()
+  }
+
+  @Get('/:id')
+  async getById(@Param() paramsId: RequestById) {
+    try {
+      return await this.prisma.todoTask.findMany({
+        where: { id: { in: [paramsId.id] } },
+      })
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Cannot proceed with request. Please try again',
+      )
+    }
   }
 }
